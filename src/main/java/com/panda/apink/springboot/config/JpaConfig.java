c16d5c1482222dbca3db74b6268faf8d9cbee4e6
apinkpanda722 <54763065+apinkpanda722@users.noreply.github.com>
2020-03-15 15:25:26 +0900

    oauth적용

---
+++ src/main/java/com/panda/apink/springboot/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.panda.apink.springboot.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
--- src/main/java/com/panda/apink/springboot/web/IndexController.java
+++ src/main/java/com/panda/apink/springboot/web/IndexController.java
@@ -1,21 +1,33 @@
 package com.panda.apink.springboot.web;
 
+import com.panda.apink.springboot.config.auth.dto.LoginUser;
+import com.panda.apink.springboot.config.auth.dto.SessionUser;
 import com.panda.apink.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import com.panda.apink.springboot.service.PostsService;
+import org.h2.engine.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
+
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+
+        if (user != null) {
+            model.addAttribute("apink", user.getName());
+        }
         return "index";
     }
 
